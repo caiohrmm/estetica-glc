90f79c0cdf93b04813230fb12b1a11563423f5c5
Lucas Rezende <lucasffrezende@gmail.com>
2024-01-22 19:52:53 -0300

    Projeto criado

---
+++ src/main/java/br/com/glc/esteticaglc/EsteticaGlcApplication.java
@@ -0,0 +1,13 @@
+package br.com.glc.esteticaglc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsteticaGlcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsteticaGlcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/glc/esteticaglc/EsteticaGlcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.glc.esteticaglc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsteticaGlcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
