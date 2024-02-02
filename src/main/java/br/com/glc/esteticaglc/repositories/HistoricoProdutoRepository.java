36f719e08bc83d4cf0bc3cba7e46fd4c46f327bd
Lucas Rezende <lucasffrezende@gmail.com>
2024-02-02 14:08:38 -0300

    Metodo findAll sobrescrito para selecionar todos historicos de produtos, cujo atributo Produto esta ativo

--- src/main/java/br/com/glc/esteticaglc/repositories/HistoricoProdutoRepository.java
+++ src/main/java/br/com/glc/esteticaglc/repositories/HistoricoProdutoRepository.java
@@ -2,6 +2,13 @@ package br.com.glc.esteticaglc.repositories;
 
 import br.com.glc.esteticaglc.entities.HistoricoProduto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface HistoricoProdutoRepository extends JpaRepository<HistoricoProduto, Long> {
+
+    @Query("SELECT hp FROM HistoricoProduto hp WHERE hp.produto.ativo = true")
+    List<HistoricoProduto> findAll();
+
 }
