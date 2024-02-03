d9f5f7ce10ef3e374533e4fa97c2242d1b8f989f
Lucas Rezende <lucasffrezende@gmail.com>
2024-02-02 22:11:40 -0300

    Adicao de metodos para calculo de valores totais e parciais

--- src/main/java/br/com/glc/esteticaglc/services/ServicoService.java
+++ src/main/java/br/com/glc/esteticaglc/services/ServicoService.java
@@ -1,8 +1,11 @@
 package br.com.glc.esteticaglc.services;
 
 import br.com.glc.esteticaglc.entities.Produto;
+import br.com.glc.esteticaglc.entities.ProdutoServico;
 import br.com.glc.esteticaglc.entities.Servico;
 import br.com.glc.esteticaglc.repositories.ServicoRepository;
+import br.com.glc.esteticaglc.utils.GrowlView;
+import br.com.glc.esteticaglc.utils.enums.MessageEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -27,6 +30,23 @@ public class ServicoService {
 
     public void inserir(Servico servico) {
         servicoRepository.save(servico);
+
+        GrowlView.showInfo(MessageEnum.MSG_SUCESSO.getMsg(), MessageEnum.MSG_SALVO_SUCESSO.getMsg());
+    }
+
+    public Double calcularValorTotalProdutos(List<ProdutoServico> produtoServicoList) {
+        Double valorTotalProdutos = 0.0;
+
+        for (ProdutoServico produtoSelecionado : produtoServicoList) {
+            valorTotalProdutos += produtoSelecionado.getProduto().getPrecoUnitario() * produtoSelecionado.getQuantidade();
+        }
+
+        return valorTotalProdutos;
+    }
+
+    public Double calcularValorTotal(Servico servico) {
+        Double valorTotal = servico.getValorTotalProdutos() + servico.getValorParcial();
+        return valorTotal;
     }
 
 }
